bca5310a80f5d6c3035f60f39b7d83582553bd52
Bohdan Kostko <bohdan.kostko@gmail.com>
2016-03-14 17:43:08 +0100

    fix(rollbar): remove restify default path

--- modules/rollbar/routes/routes.js
+++ modules/rollbar/routes/routes.js
@@ -4,10 +4,6 @@ module.exports = function(rollbarController) {
   return {
     registerRoutes(server) {
       server.post('/rollbar/webhook', rollbarController);
-      server.get('/', (req, res, next) => {
-        res.send('ok');
-        next();
-      });
     }
   };
 };
